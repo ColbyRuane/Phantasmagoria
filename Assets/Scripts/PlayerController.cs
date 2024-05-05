da596cb465fb8470a54887b50650923cdf234e6e
ColbyRuane <ruane000@gmail.com>
2024-05-04 19:15:21 -0500

    Added Player Combat Physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,21 +10,27 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float rotationSpeed = 500f;
     [SerializeField] float jumpStrength = 5f;
     [SerializeField] float jumpButtonGracePeriod;
+    Quaternion targetRotation;
+    float trueSpeed;
+    float threshold;
 
     [Header("Ground Check Settings")]
     [SerializeField] float groundCheckRadius = 0.2f;
     [SerializeField] Vector3 groundCheckOffset;
     [SerializeField] LayerMask groundLayer;
-
-    float trueSpeed;
-    float threshold;
     float ySpeed;
-    Quaternion targetRotation;
     bool isGrounded;
     float gravity = -9.81f;
     float? lastGroundedTime;
     float? jumpButtonPressedTime;
     bool isJumping;
+
+    [Header("Combat")]
+    [SerializeField] float attackRadius;
+    public Transform attackPoint;
+    public LayerMask enemyLayer;
+    float lastAttack = -3f;
+    float attackCooldown = 3f;
 
     CameraController cameraController;
     CharacterController characterController;
@@ -40,8 +46,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // get player input
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         // build vector using player input
         var directionInput = new Vector3(horizontal, 0, vertical).normalized;
         // check for walk or run
@@ -130,125 +136,32 @@ public class PlayerController : MonoBehaviour
 
         // switch animations (dampened through animator)
         animator.SetFloat("moveAmount", threshold, 0.15f, Time.deltaTime);
+
+        Attack();
+    }
+
+    private void Attack()
+    {
+        // guarantees that an attack is performed while not in midair
+        if (Input.GetMouseButtonDown(0) && isGrounded && (Time.time - lastAttack >= attackCooldown))
+        {
+            animator.SetTrigger("isAttack");
+            // grab instance of attack trigger to compare with attack cooldown
+            lastAttack = Time.time;
+            // detect enemies in range of attack
+            Collider[] hitThing = Physics.OverlapSphere(attackPoint.position, attackRadius, enemyLayer);
+
+            foreach (Collider thing in hitThing)
+            {
+                Debug.Log(thing.name + " was hit!");
+            }
+        }
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = new Color(0, 1, 0, 0.5f);
         Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
+        Gizmos.DrawSphere(attackPoint.position, attackRadius);
     }
 }
-
-/*
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{
-    [Header("Locomotion")]
-    [SerializeField] float walkSpeed = 2f;
-    [SerializeField] float runSpeed = 8f;
-    [SerializeField] float rotationSpeed = 500f;
-    [SerializeField] float jumpStrength = 2f;
-    [SerializeField] float jumpGracePeriod = 2f;
-
-    [Header("Ground Check Settings")]
-    [SerializeField] float groundCheckRadius = 0.2f;
-    [SerializeField] Vector3 groundCheckOffset;
-    [SerializeField] LayerMask groundLayer;
-
-    float trueSpeed;
-    float threshold;
-    float ySpeed;
-    Quaternion targetRotation;
-    bool isGrounded;
-    float gravity = -9.81f;
-    Vector3 velocity;
-
-    CameraController cameraController;
-    CharacterController characterController;
-    Animator animator;
-
-    private void Awake()
-    {
-        cameraController = Camera.main.GetComponent<CameraController>();
-        animator = GetComponent<Animator>();
-        characterController = GetComponent<CharacterController>();
-    }
-
-    void Update()
-    {
-        // get player input
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        // build vector using player input
-        var directionInput = new Vector3(horizontal, 0, vertical).normalized;
-        // check for walk or run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-        // movement dependent on camera rotation - DO NOT ACCOUNT FOR VERTICAL ROTATION
-        var moveDirection = cameraController.PlanarRotation * directionInput;
-
-        // adjust velocity for gravity if player is not grounded
-        isGrounded = Physics.CheckSphere(transform.TransformPoint(groundCheckOffset),
-            groundCheckRadius, groundLayer);
-        if (isGrounded && velocity.y < 0)
-        {
-            // keep track of time for jumping animations
-            animator.SetBool("isGrounded", true);
-            velocity.y = -0.5f;
-        }
-        else
-        {
-            animator.SetBool("isGrounded", false);
-        }
-
-        // check for input before adjusting character speed and rotation
-        if (directionInput.magnitude > 0)
-        {
-            if (isRunning)
-            {
-                trueSpeed = runSpeed;
-                threshold = 1;
-            }
-            else
-            {
-                trueSpeed = walkSpeed;
-                threshold = 0.5f;
-            }
-
-            // change direction character is facing based on input
-            targetRotation = Quaternion.LookRotation(moveDirection);
-        }
-        else
-        {
-            threshold = 0;
-        }
-
-        // change horizontal position, frame-independent
-        characterController.Move(moveDirection * trueSpeed * Time.deltaTime);
-
-        // change vertical position, frame-independent
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpStrength * -2f * gravity);
-        }
-        // gravity modifier
-        velocity.y += gravity * 1.5f * Time.deltaTime;
-        characterController.Move(velocity * Time.deltaTime);
-
-        // smooth player rotation
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, 
-            targetRotation, rotationSpeed * Time.deltaTime);
-
-        // switch animations (dampened through animator)
-        animator.SetFloat("moveAmount", threshold);
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = new Color(0, 1, 0, 0.5f);
-        Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
-    }
-}
-*/
